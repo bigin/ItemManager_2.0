095bfe8a22c00e083e9d9eecb1eac5ce079b8de2
Juri Ehret <juri.ehret@gmail.com>
2016-08-17 09:26:50 +0200

    cleaning input

--- imanager/lib/processors/inputs/InputImageupload.php
+++ imanager/lib/processors/inputs/InputImageupload.php
@@ -4,6 +4,7 @@ class InputImageupload implements InputInterface
 	protected $values;
 	protected $field;
 
+
 	public function __construct(Field $field)
 	{
 		$this->field = $field;
@@ -17,11 +18,6 @@ class InputImageupload implements InputInterface
 		$this->values->imageurl = array();
 		$this->values->imagefullurl = array();
 		$this->values->imagetitle = array();
-
-		/*$this->values->thumbpath = array();
-		$this->values->thumbfullpath = array();
-		$this->values->thumburl = array();
-		$this->values->thumbfullurl = array();*/
 
 		$this->positions = array();
 		$this->titles = array();
@@ -74,11 +70,6 @@ class InputImageupload implements InputInterface
 			$temp_arr[$i]->imagefullurl = 'data/uploads/imanager/'.$basedir.'/'.$base;
 			$temp_arr[$i]->imagetitle = $title;
 
-			/*$temp_arr[$i]->thumbpath = $value.'thumbnail/';
-			$temp_arr[$i]->thumbfullpath = $value.'thumbnail/'.$base;
-			$temp_arr[$i]->thumburl = $this->getFullUrl().'/data/uploads/imanager/'.$basedir.'/thumbnail/'.$base;
-			$temp_arr[$i]->thumbfullurl = $this->getFullUrl().'/data/uploads/imanager/'.$basedir.'/thumbnail/'.$base;*/
-
 			$i++;
 		}
 
@@ -94,11 +85,6 @@ class InputImageupload implements InputInterface
 			$this->values->imageurl[] = $temp_arr[$key]->imageurl;
 			$this->values->imagefullurl[] = $temp_arr[$key]->imagefullurl;
 			$this->values->imagetitle[] = $temp_arr[$key]->imagetitle;
-
-			/*$this->values->thumbpath[] = $temp_arr[$key]->thumbpath;
-			$this->values->thumbfullpath[] = $temp_arr[$key]->thumbfullpath;
-			$this->values->thumburl[] = $temp_arr[$key]->thumburl;
-			$this->values->thumbfullurl[] = $temp_arr[$key]->thumbfullurl;*/
 
 		}
 		// delete empty config file
@@ -127,17 +113,6 @@ class InputImageupload implements InputInterface
 		}
 	}
 
-	protected function getFullUrl()
-	{
-		return IM_SITE_URL;
-		/*$https = !empty($_SERVER['HTTPS']) && strcasecmp($_SERVER['HTTPS'], 'on') === 0 ||
-			!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-			strcasecmp($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') === 0;
-		return
-			($https ? 'https://' : 'http://').
-			(!empty($_SERVER['REMOTE_USER']) ? $_SERVER['REMOTE_USER'].'@' : '').
-			(isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : ($_SERVER['SERVER_NAME'].
-				($https && $_SERVER['SERVER_PORT'] === 443 ||
-				$_SERVER['SERVER_PORT'] === 80 ? '' : ':'.$_SERVER['SERVER_PORT'])));*/
-	}
+
+	protected function getFullUrl() {return IM_SITE_URL;}
 }
